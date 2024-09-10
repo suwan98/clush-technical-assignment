192161ca1385becfd593e7a86f1ead8489632f7b
Suwan <imissyou5466@gmail.com>
2024-09-10 13:47:20 +0900

    ✨ feat : 체크 완료 여부에 따른 필터링 리스트 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,24 @@ import {UnorderedListOutlined, CheckSquareOutlined} from "@ant-design/icons";
 import useTodo from "./hooks/useTodo";
 
 function App() {
-  const {todos, addTodo, toggleTodo, deleteTodo} = useTodo();
+  const {todos, addTodo, toggleTodo, deleteTodo, filter, setFilter} = useTodo();
+
+  const handleMenuClick = (key: string) => {
+    setFilter(key as "all" | "completed");
+  };
 
   return (
     <Layout style={{minHeight: "100vh"}}>
       <Layout.Sider breakpoint="lg" collapsedWidth="0">
-        <Menu theme="dark" mode="inline">
-          <Menu.Item key="1" icon={<UnorderedListOutlined />}>
+        <Menu
+          theme="dark"
+          mode="inline"
+          selectedKeys={[filter]}
+          onClick={({key}) => handleMenuClick(key)}>
+          <Menu.Item key="all" icon={<UnorderedListOutlined />}>
             모든 할 일
           </Menu.Item>
-          <Menu.Item key="2" icon={<CheckSquareOutlined />}>
+          <Menu.Item key="completed" icon={<CheckSquareOutlined />}>
             완료된 할 일
           </Menu.Item>
         </Menu>
